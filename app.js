503333c90a55f1ec8c59202e3dfb2aac9971e2b9
GiovanniOuvenis <s_second@hotmail.com>
2022-08-24 20:53:13 +0200

    experimantel changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandling = require("./middleware/error-handler");
 const cors = require("cors");
-
+// Lets assoume the code was added
 app.set("trust proxy", 1);
 app.use(cors());
 app.use(express.json());
